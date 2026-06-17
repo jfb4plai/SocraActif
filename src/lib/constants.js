c5570b3c2002af39065bf1ab3ba3f7ae84d37ed5
Jean-François Beguin <jf.beguin@outlook.com>
2026-06-17 12:02:18 +0200

    feat: Supabase client + app constants

---
+++ src/lib/constants.js
@@ -0,0 +1,22 @@
+export const DEFAULTS = {
+  validation_threshold: 70,
+  blocking_mode: 'soft',
+  max_socratic_turns: 4
+}
+
+export const ERROR_TYPES = {
+  faute: 'faute',
+  technique: 'technique',
+  technologie: 'technologie',
+  correct: 'correct'
+}
+
+export const SCAFFOLDING = {
+  explicite: 'explicite',
+  inductif: 'inductif'
+}
+
+export const MODE = {
+  autonomie: 'autonomie',
+  projection: 'projection'
+}
